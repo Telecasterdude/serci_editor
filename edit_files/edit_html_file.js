e9666939915c60f35f0b37d1b4c7cc1d393bb071
Daniel Watt <dano4owl@gmail.com>
2019-08-23 17:13:43 +1200

    text can now be imported correctly, and added stylesheet for first time

--- edit_files/edit_html_file.js
+++ edit_files/edit_html_file.js
@@ -23,17 +23,31 @@ function change_line_numbers(numlines_to_add) {
 }
 
 
+
+function add_character_element(element, index, char) {
+    /* Takes an element, an index and a character string and places the character at the required 
+     * index in the element */
+    
+    let inner_text = element.textContent;
+    
+    let inner_char_list = inner_text.split("");
+    inner_char_list.splice(index, 0, char);
+    element.textContent = inner_text;
+}
+
+
+
 function keypress_handler(key) {
-    /* Takes a key and changes the display of the text editor as nessesary */
+    /* Takes a key, adds the corresponding character to the text, and changes the display 
+     * of the text editor as nessesary */
     
     let line_numbers = document.querySelector("#line-numbers");
     let code = document.querySelector('#code-area'); 
     let selection = window.getSelection();
-
     
     if (key === "Enter") {
         change_line_numbers(1);
-    
+        
      /* The user has highlighted some text and pressed backspace */
     } else if (key === "Backspace" && selection.toString().length > 0) {  /* this needs to come before normal backspace */
         let selected_text = selection.toString();                         /* or else there is a bug when selecting blank enters */
@@ -47,7 +61,6 @@ function keypress_handler(key) {
         change_line_numbers(-1);
     }  
     
-    
     //this is a temprorary fix for an obscure bug
     if (!(key === "Backspace" && selection.toString().length > 0)) {
         let required_num_lines = Math.max((code.innerHTML.match(/<\/div>|^\s<div>|<br>^<\/div>/g) || []).length, 1) - (key === "Backspace") * 1;
@@ -59,6 +72,45 @@ function keypress_handler(key) {
             edit_state.current_number_of_lines = required_num_lines;
         }
     }
+}
+
+
+
+function populate_code(file) {
+    let file_contents = `<!DOCTYPE html>
+aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./../stylesheet.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">aaaaaaa<link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">aaaaaaa
+        <title>SERCI Web Editor</title>
+    </head>
+    <body>
+        <h1 class="subtitle-text" id="file-title">Loading...</h1>
+        <div class="code-section">
+            <div id="line-numbers" class="code-line">1</div>
+            <div id="code-area" class="code-line" type="text" contenteditable="true"></div>
+        </div>
+    </body>
+    <script src="edit_html_file.js"></script>
+</html>`;
+    
+    let lines_in_file = file_contents.match(/\n/g).length + 1;
+    change_line_numbers(lines_in_file-1);
+    let code = document.querySelector('#code-area'); 
+    line_list = file_contents.split("\n");
+    
+    let i;
+    for (i = 0; i < line_list.length-1; i++) {
+        let line = document.createElement('div');
+        line.textContent = `${line_list[i]}`;
+        code.appendChild(line);
+    }
+    
+    let last_div = document.createElement('div');
+    last_div.textContent = line_list[i];
+    code.appendChild(last_div);
 }
 
 
@@ -76,3 +128,4 @@ document.querySelector('#code-area').addEventListener('paste', (e) => {
     change_line_numbers(number_of_enters);
 });
 
+populate_code("wrew");
