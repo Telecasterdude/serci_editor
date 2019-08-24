a09138b3f69d6f105c62f8df71fe3ecd3651ec5a
Daniel Watt <dano4owl@gmail.com>
2019-08-25 02:50:45 +1200

    improvements to code editor made, and now loads in the actual code from the server

--- edit_files/edit_html_file.js
+++ edit_files/edit_html_file.js
@@ -1,5 +1,6 @@
-let edit_state = {current_number_of_lines : 1, 
-                  is_pasting_data : false}; 
+const {PythonShell} = require('python-shell');
+
+let edit_state = {current_number_of_lines : 1}; 
 
 function change_line_numbers(numlines_to_add) {
     /* Takes an integer specifying the number of lines to add. If this number 
@@ -44,6 +45,8 @@ function keypress_handler(key) {
     let line_numbers = document.querySelector("#line-numbers");
     let code = document.querySelector('#code-area'); 
     let selection = window.getSelection();
+        
+    
     
     if (key === "Enter") {
         change_line_numbers(1);
@@ -63,7 +66,9 @@ function keypress_handler(key) {
     
     //this is a temprorary fix for an obscure bug
     if (!(key === "Backspace" && selection.toString().length > 0)) {
-        let required_num_lines = Math.max((code.innerHTML.match(/<\/div>|^\s<div>|<br>^<\/div>/g) || []).length, 1) - (key === "Backspace") * 1;
+        let line_height = 14 * 1.3; //this is defined in the css
+        let required_num_lines = Math.ceil(code.scrollHeight / line_height) - (key === "Backspace");
+        
         if (edit_state.current_number_of_lines < required_num_lines) {
             line_numbers.innerHTML = `${1}`;
             for (let i = 1; i < required_num_lines; i++) {
@@ -75,31 +80,24 @@ function keypress_handler(key) {
 }
 
 
-
-function populate_code(file) {
-    let file_contents = `<!DOCTYPE html>
-aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="./../stylesheet.css">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">aaaaaaa<link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">aaaaaaa
-        <title>SERCI Web Editor</title>
-    </head>
-    <body>
-        <h1 class="subtitle-text" id="file-title">Loading...</h1>
-        <div class="code-section">
-            <div id="line-numbers" class="code-line">1</div>
-            <div id="code-area" class="code-line" type="text" contenteditable="true"></div>
-        </div>
-    </body>
-    <script src="edit_html_file.js"></script>
-</html>`;
+function populate_page(filename) {
+    /* Takes a filename and populates the page with it */
     
-    let lines_in_file = file_contents.match(/\n/g).length + 1;
-    change_line_numbers(lines_in_file-1);
+    let options = {
+        scriptPath : './python_scripts/',
+        args : [filename]
+    }
+    
+    PythonShell.run('download_file.py', options, (error, output) => {console.log(output); populate_code(output);})
+}
+
+
+
+function populate_code(line_list) {
+    /* Takes a list containing each line of the file and populates the code section of 
+     * the page with the contents of that file */
+    
     let code = document.querySelector('#code-area'); 
-    line_list = file_contents.split("\n");
     
     let i;
     for (i = 0; i < line_list.length-1; i++) {
@@ -111,6 +109,14 @@ aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aa
     let last_div = document.createElement('div');
     last_div.textContent = line_list[i];
     code.appendChild(last_div);
+    
+    let line_height = 14 * 1.3; //this is defined in the css
+    let number_of_lines_needed = Math.ceil(code.scrollHeight / line_height);
+    change_line_numbers(number_of_lines_needed-1);
+    
+    let line_numbers = document.querySelector('#line-numbers');
+    let line_numbers_width = line_numbers.offsetWidth;
+    code.style.width = `calc(100% - (${line_numbers_width + 20}px)`; //10px padding on each side of #code-area
 }
 
 
@@ -128,4 +134,4 @@ document.querySelector('#code-area').addEventListener('paste', (e) => {
     change_line_numbers(number_of_enters);
 });
 
-populate_code("wrew");
+populate_page("./html/aims_and_scope.html");
